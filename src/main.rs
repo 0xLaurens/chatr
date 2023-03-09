752610358376d07e87e4253dd4e518ddb4766663
lauwIV <64641417+lauwIV@users.noreply.github.com>
2023-03-09 21:52:28 +0100

    project -> websocket

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,40 @@
+use std::net::SocketAddr;
 use axum::{
     routing::get,
     Router,
+    extract::ws::{WebSocketUpgrade, WebSocket},
+    response::{Response},
 };
+
 
 #[tokio::main]
 async fn main() {
   let app = Router::new()
-      .route("/", get(|| async {"Hello World"}));
+      .route("/", get(|| async {"Hello World"}))
+      .route("/ws", get(handler));
 
-  axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+  let addr = SocketAddr::from(([0,0,0,0], 3000));
+  axum::Server::bind(&addr)
       .serve(app.into_make_service())
       .await
-      .unwrap()
+      .unwrap();
 }
+
+async fn handler(ws: WebSocketUpgrade) -> Response {
+    ws.on_upgrade(handle_socket)
+}
+
+async fn handle_socket(mut socket: WebSocket) {
+    while let Some(msg) = socket.recv().await {
+        let msg = if let Ok(msg) = msg {
+            msg
+        } else {
+            return;
+        };
+
+        if socket.send(msg).await.is_err() {
+            return;
+        }
+    }
+}
+
