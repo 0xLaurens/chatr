e1815fc7f24b7047dbfc1fb88971c402301c743a
lauwIV <64641417+lauwIV@users.noreply.github.com>
2023-03-20 11:02:16 +0100

    client (.env) -> changed to dynamic

--- client/src/routes/+page.ts
+++ client/src/routes/+page.ts
@@ -1,9 +1,9 @@
 import type {PageLoad} from './$types';
-import {PUBLIC_API_URL} from "$env/static/public";
+import {env} from "$env/dynamic/public";
 
 export const load: PageLoad = async ({fetch}) => {
     try {
-        const res = await fetch(`http://${PUBLIC_API_URL}/rooms`);
+        const res = await fetch(`http://${env.PUBLIC_API_URL}/rooms`);
         return await res.json();
     } catch (e) {
         return {
